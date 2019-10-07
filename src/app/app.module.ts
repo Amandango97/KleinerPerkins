b1b5ceffd69b3db41091716ccff21dccea832db3
Amandango97 <amngo@wharton.upenn.edu>
2019-10-07 01:20:59 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -12,7 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, AnimateOnScrollModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Platform } from '@ionic/angular';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('elementState', [
+      state('opaque', style({
+        opacity: 1
+      })),
+      state('transparent', style({
+        opacity: 0
+      })),
+      transition('transparent => opaque',animate('0s 0.5s'))
+    ]),
+    trigger('elementState2', [
+      state('opaque', style({
+        opacity: 1
+      })),
+      state('transparent', style({
+        opacity: 0
+      })),
+      transition('transparent => opaque', animate('0.1s 0.5s'))
+    ]),
+    trigger('elementState3', [
+      state('opaque', style({
+        opacity: 1
+      })),
+      state('transparent', style({
+        opacity: 0
+      })),
+      transition('transparent => opaque', animate('0.5s 1s'))
+    ]),
+    trigger('elementState4', [
+      state('opaque', style({
+        opacity: 1
+      })),
+      state('transparent', style({
+        opacity: 0
+      })),
+      transition('transparent => opaque', animate('1s 1s'))
+    ]),
+  ]
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public platform: Platform) {}
+
+  state = "transparent";
+
+  ngOnInit() {
+    this.platform.ready().then(() => { 
+      this.state = "opaque";
+      });
+  }
+
+
+
 
 }
